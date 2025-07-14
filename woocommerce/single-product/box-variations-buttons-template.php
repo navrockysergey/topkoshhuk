03775368bbe1b1ce0f251f64047b8edad5a5a029
Ushakov-Demon <dimon.milion2@gmail.com>
2025-07-14 16:36:47 +0300

    Fix qty buttons logic

--- woocommerce/single-product/box-variations-buttons-template.php
+++ woocommerce/single-product/box-variations-buttons-template.php
@@ -3,21 +3,22 @@ defined( 'ABSPATH' ) || exit;
 
 global $product;
 $product_id       = $product->get_id();
-$in_box    = intval( get_post_meta( $product_id, '_box_quantity', true ) );
-$in_cart   = intval( apply_filters( 'get_cart_product_count', $product_id ) );
+$in_box      = intval( get_post_meta( $product_id, '_box_quantity', true ) );
+$in_cart     = intval( apply_filters( 'get_cart_product_count', $product_id ) );
 $is_in_stock = $product->is_in_stock();
 
-$retail_active    = $in_cart < $in_box ? 'active' : '';
-$wholesale_active = $in_cart > $in_box ? 'active' : '';
+$retail_active    = 0 < $in_cart && $in_cart < $in_box ? 'active' : '';
+$wholesale_active = 'active' !== $retail_active ? 'active' : '';
+
 ?>
 <?php if ( $is_in_stock ) : ?>
     <div class="box-variations">
-        <button class="box-variation wholesale active" data-qty-suffix="<?php _e('ящ.'); ?>">
+        <button class="box-variation wholesale <?php echo $wholesale_active?>" data-qty-suffix="<?php _e('ящ.'); ?>">
             <i class="icon-box-24"></i>
             <?php _e('Ящики'); ?>
         </button>
         
-        <button class="box-variation retail" data-qty-suffix="<?php _e('шт.'); ?>">
+        <button class="box-variation retail <?php echo $retail_active?>" data-qty-suffix="<?php _e('шт.'); ?>">
             <i class="icon-item-24"></i>
             <?php _e('Штуки'); ?>
         </button>
--- woocommerce/single-product/fake-add-to-cart.php
+++ woocommerce/single-product/fake-add-to-cart.php
@@ -5,8 +5,15 @@ $in_cart_q = $args['in_cart_q'];
 $product   = $args['product'];
 
 if ( $val > 0 && $in_box > 0 && $val >= $in_box ) {
-    $val = $val/$in_box;
+    $val = round( $val/$in_box, 2 );
 }
+
+$retail_step = apply_filters( 'woocommerce_quantity_input_step', 1, $product );
+
+if ( $in_cart_q > $in_box ) {
+   $retail_step = $in_box;
+}
+
 ?>
 <div class="qty-container" data-in-box="<?php echo $in_box?>" data-product-id="<?php echo $product->get_id()?>">
     <button class="button button-qty qty-minus">
@@ -19,6 +26,7 @@ if ( $val > 0 && $in_box > 0 && $val >= $in_box ) {
                 'min_value'   => '0',
                 'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
                 'input_value' => $in_cart_q,
+                'step'        => $retail_step,
             )
         );
     ?>
